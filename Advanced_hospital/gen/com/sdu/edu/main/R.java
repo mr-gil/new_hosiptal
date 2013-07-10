c718e8fc341e31ccb54fe02a6d1344bdb4ae8bcf
Hell <jianyangsong@gmail.com>
2013-07-10 14:29:27 +0800

    advanced hospital add library

--- Advanced_hospital/gen/com/sdu/edu/main/R.java
+++ Advanced_hospital/gen/com/sdu/edu/main/R.java
@@ -8,13 +8,123 @@
 package com.sdu.edu.main;
 
 public final class R {
+    public static final class anim {
+        public static final int rbm_in_from_left=0x7f040000;
+        public static final int rbm_out_to_left=0x7f040001;
+    }
     public static final class attr {
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int classType=0x7f010005;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int customRegexp=0x7f010003;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int emptyAllowed=0x7f010004;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int emptyErrorString=0x7f010002;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int testErrorString=0x7f010001;
+        /** <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>regexp</code></td><td>0</td><td></td></tr>
+<tr><td><code>numeric</code></td><td>1</td><td></td></tr>
+<tr><td><code>alpha</code></td><td>2</td><td></td></tr>
+<tr><td><code>alphaNumeric</code></td><td>3</td><td></td></tr>
+<tr><td><code>email</code></td><td>4</td><td></td></tr>
+<tr><td><code>creditCard</code></td><td>5</td><td></td></tr>
+<tr><td><code>phone</code></td><td>6</td><td></td></tr>
+<tr><td><code>domainName</code></td><td>7</td><td></td></tr>
+<tr><td><code>ipAddress</code></td><td>8</td><td></td></tr>
+<tr><td><code>webUrl</code></td><td>9</td><td></td></tr>
+<tr><td><code>nocheck</code></td><td>10</td><td></td></tr>
+<tr><td><code>custom</code></td><td>11</td><td></td></tr>
+</table>
+         */
+        public static final int testType=0x7f010000;
+    }
+    public static final class color {
+        public static final int rbm_item_text_color=0x7f050001;
+        public static final int rbm_menu_background=0x7f050000;
+    }
+    public static final class dimen {
+        public static final int rbm_item_image_height=0x7f060003;
+        public static final int rbm_item_image_width=0x7f060004;
+        public static final int rbm_item_padding_leftright=0x7f060002;
+        public static final int rbm_item_padding_topbottom=0x7f060001;
+        public static final int rbm_item_text_padding_left=0x7f060006;
+        public static final int rbm_item_text_size=0x7f060005;
+        public static final int rbm_menu_width=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int alpha=0x7f070002;
+        public static final int alphaNumeric=0x7f070003;
+        public static final int creditCard=0x7f070005;
+        public static final int custom=0x7f07000b;
+        public static final int domainName=0x7f070007;
+        public static final int email=0x7f070004;
+        public static final int ipAddress=0x7f070008;
+        public static final int nocheck=0x7f07000a;
+        public static final int numeric=0x7f070001;
+        public static final int phone=0x7f070006;
+        public static final int rbm_item_icon=0x7f07000c;
+        public static final int rbm_item_text=0x7f07000d;
+        public static final int rbm_listview=0x7f07000e;
+        public static final int rbm_outside_view=0x7f07000f;
+        public static final int regexp=0x7f070000;
+        public static final int webUrl=0x7f070009;
+    }
+    public static final class layout {
+        public static final int rbm_item=0x7f030000;
+        public static final int rbm_menu=0x7f030001;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f08000a;
+        public static final int error_creditcard_number_not_valid=0x7f080005;
+        public static final int error_domain_not_valid=0x7f080007;
+        public static final int error_email_address_not_valid=0x7f080004;
+        public static final int error_field_must_not_be_empty=0x7f080003;
+        public static final int error_ip_not_valid=0x7f080008;
+        public static final int error_only_numeric_digits_allowed=0x7f080000;
+        public static final int error_only_standard_letters_are_allowed=0x7f080002;
+        public static final int error_phone_not_valid=0x7f080006;
+        public static final int error_this_field_cannot_contain_special_character=0x7f080001;
+        public static final int error_url_not_valid=0x7f080009;
     }
     public static final class style {
         /** 
@@ -38,10 +148,133 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f090001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a FormEditText.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #FormEditText_classType com.sdu.edu.main:classType}</code></td><td></td></tr>
+           <tr><td><code>{@link #FormEditText_customRegexp com.sdu.edu.main:customRegexp}</code></td><td></td></tr>
+           <tr><td><code>{@link #FormEditText_emptyAllowed com.sdu.edu.main:emptyAllowed}</code></td><td></td></tr>
+           <tr><td><code>{@link #FormEditText_emptyErrorString com.sdu.edu.main:emptyErrorString}</code></td><td></td></tr>
+           <tr><td><code>{@link #FormEditText_testErrorString com.sdu.edu.main:testErrorString}</code></td><td></td></tr>
+           <tr><td><code>{@link #FormEditText_testType com.sdu.edu.main:testType}</code></td><td></td></tr>
+           </table>
+           @see #FormEditText_classType
+           @see #FormEditText_customRegexp
+           @see #FormEditText_emptyAllowed
+           @see #FormEditText_emptyErrorString
+           @see #FormEditText_testErrorString
+           @see #FormEditText_testType
+         */
+        public static final int[] FormEditText = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
+            0x7f010004, 0x7f010005
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.sdu.edu.main.R.attr#classType}
+          attribute's value can be found in the {@link #FormEditText} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:classType
+        */
+        public static final int FormEditText_classType = 5;
+        /**
+          <p>This symbol is the offset where the {@link com.sdu.edu.main.R.attr#customRegexp}
+          attribute's value can be found in the {@link #FormEditText} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:customRegexp
+        */
+        public static final int FormEditText_customRegexp = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.sdu.edu.main.R.attr#emptyAllowed}
+          attribute's value can be found in the {@link #FormEditText} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:emptyAllowed
+        */
+        public static final int FormEditText_emptyAllowed = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.sdu.edu.main.R.attr#emptyErrorString}
+          attribute's value can be found in the {@link #FormEditText} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:emptyErrorString
+        */
+        public static final int FormEditText_emptyErrorString = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.sdu.edu.main.R.attr#testErrorString}
+          attribute's value can be found in the {@link #FormEditText} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:testErrorString
+        */
+        public static final int FormEditText_testErrorString = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.sdu.edu.main.R.attr#testType}
+          attribute's value can be found in the {@link #FormEditText} array.
+
+
+          <p>Must be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>regexp</code></td><td>0</td><td></td></tr>
+<tr><td><code>numeric</code></td><td>1</td><td></td></tr>
+<tr><td><code>alpha</code></td><td>2</td><td></td></tr>
+<tr><td><code>alphaNumeric</code></td><td>3</td><td></td></tr>
+<tr><td><code>email</code></td><td>4</td><td></td></tr>
+<tr><td><code>creditCard</code></td><td>5</td><td></td></tr>
+<tr><td><code>phone</code></td><td>6</td><td></td></tr>
+<tr><td><code>domainName</code></td><td>7</td><td></td></tr>
+<tr><td><code>ipAddress</code></td><td>8</td><td></td></tr>
+<tr><td><code>webUrl</code></td><td>9</td><td></td></tr>
+<tr><td><code>nocheck</code></td><td>10</td><td></td></tr>
+<tr><td><code>custom</code></td><td>11</td><td></td></tr>
+</table>
+          @attr name android:testType
+        */
+        public static final int FormEditText_testType = 0;
+    };
 }
